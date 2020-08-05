b1dec9941ec171febbebb38d7f7654e5932ad8ce
arturgawlik <artur.gawlik@icloud.com>
2020-08-05 23:57:48 +0200

    working with login view

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,17 +3,28 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { Routes, RouterModule } from '@angular/router';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent }
 ];
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, LoginFormComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    FlexLayoutModule,
+    NzCardModule
   ]
 })
 export class LoginModule { }
